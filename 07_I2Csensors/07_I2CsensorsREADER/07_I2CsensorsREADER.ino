d1c24b828b3e972aae0fc97c52fa03452e5b572e
Daniel <159121711+GM-Daniel@users.noreply.github.com>
2025-02-14 13:31:58 +0100

    Editing READER to at least compile

--- 07_I2Csensors/07_I2CsensorsREADER/07_I2CsensorsREADER.ino
+++ 07_I2Csensors/07_I2CsensorsREADER/07_I2CsensorsREADER.ino
@@ -4,9 +4,22 @@
 const byte DEVICE_ADDRESS = 8;
 byte row = 0;
 
+#define ROWS 5 
+#define COLS 3
+float matrix[ROWS][COLS];
+byte buffer[4];
+int current_row;
+
+
 void receiveEvent(int bytes) {
   row = Wire.read();  
-
+  Wire.read();
+  for (int b = 0; b < 4; b++) {
+      if (Wire.available()) {
+        buffer[b] = Wire.read();
+      }
+  }
+  memcpy(&current_row, buffer, 4);
   for (int j = 0; j < 3; j++) {
     byte buffer[4];  
     for (int b = 0; b < 4; b++) {
@@ -14,10 +27,19 @@ void receiveEvent(int bytes) {
         buffer[b] = Wire.read();
       }
     }
-    memcpy(&matriz[fila_actual][j], buffer, 4);  
+    memcpy(&matrix[current_row][j], buffer, 4);  
   }
-  Serial.print("Fila recibida: ");
-  Serial.println(fila_actual);
+  Serial.print("Row captured: ");
+  Serial.println(current_row);
+  for (int i = 0; i < 3; i++) {
+    Serial.print(matrix[current_row][i]);  
+    if (i < 2) {
+      Serial.print(", "); 
+    }
+  }
+  
+  // Print a newline after the first row
+  Serial.println();
 }
 
 void setup() {
