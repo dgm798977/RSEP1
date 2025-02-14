71df90d2ad7539a9bb575483d633e831481a5468
Daniel <159121711+GM-Daniel@users.noreply.github.com>
2025-02-14 08:33:21 +0100

    Editing internal structure of folder to explore better options (v2)

---
+++ 03_PWM/03_PWM/03_PWM.ino
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <TimerOne.h>
+
+
+int pinSensor = A0;
+int pinPWM = 9;  
+long frec = 5000;
+int PMWvalue;
+int value;
+
+void setup() {
+  Timer1.initialize(1000000 / frec);
+}
+
+void loop() {
+  value = analogRead(pinSensor);
+  PWMvalue = map(value, 0, 1023, 0, 255);
+  Timer1.pwm(pinPWM, PWMvalue);
+}
